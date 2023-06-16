30e8121c35b14c21e0286aeccaf54cfbfd76a26e
Bhargav-LNSN <116058139+Bhargav-LNSN@users.noreply.github.com>
2023-06-16 10:10:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "myinsta" {
-  ami           = "ami-09988af04120b3591" # us-west-2
+  ami           = "ami-09988af04120b3591" 
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
